2da5301713cb0594c8f44c0ec3a4cd252b5c672c
Rodrigo Silva Ferreira <rodrigosferreira0@gmail.com>
2023-05-31 23:07:11 -0300

    requisito 3

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -3,10 +3,43 @@ import StarContext from '../context/StarContext';
 
 export default function Table() {
   const { starsData } = useContext(StarContext);
-  const [filterStar, setFilterText] = useState('');
+  const [filterText, setFilterText] = useState('');
+  const [filterColumn, setFilterColumn] = useState('population');
+  const [filterComparison, setFilterComparison] = useState('maior que');
+  const [filterValue, setFilterValue] = useState(0);
+  const [aFilter, setAFilter] = useState(false);
 
   const filterStarName = starsData.filter((star) => star.name.toLowerCase()
-    .includes(filterStar.toLowerCase()));
+    .includes(filterText.toLowerCase())).filter((star) => {
+    if (!aFilter) return star;
+    if (filterComparison === 'maior que') {
+      return star[filterColumn] > Number(filterValue);
+    }
+    if (filterComparison === 'menor que') {
+      return star[filterColumn] < Number(filterValue);
+    }
+    if (filterComparison === 'igual a') {
+      return star[filterColumn] === filterValue;
+    }
+    return true;
+  });
+
+  /*   const outroFilter = filterStarName.filter((star) => {
+    if (filterComparison === 'maior que') {
+      return star[filterColumn] > Number(filterValue);
+    }
+    if (filterComparison === 'menor que') {
+      return star[filterColumn] < Number(filterValue);
+    }
+    if (filterComparison === 'igual a') {
+      return star[filterColumn] === Number(filterValue);
+    }
+    return true;
+  });
+
+  console.log('====================================');
+  console.log(outroFilter);
+  console.log('===================================='); */
 
   return (
     <div>
@@ -14,10 +47,48 @@ export default function Table() {
         type="text"
         name="filterStar"
         data-testid="name-filter"
-        value={ filterStar }
+        value={ filterText }
         onChange={ (e) => setFilterText(e.target.value) }
         placeholder="Digite o nome do planeta"
       />
+      <div>
+        <select
+          name="filterColumn"
+          value={ filterColumn }
+          data-testid="column-filter"
+          onChange={ (e) => setFilterColumn(e.target.value) }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+        <select
+          value={ filterComparison }
+          name="filterComparison"
+          onChange={ (e) => setFilterComparison(e.target.value) }
+          data-testid="comparison-filter"
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          value={ filterValue }
+          onChange={ (e) => setFilterValue(e.target.value) }
+          data-testid="value-filter"
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ () => setAFilter(true) }
+        >
+          Filtrar
+
+        </button>
+      </div>
       <table>
         <thead>
           <tr>
